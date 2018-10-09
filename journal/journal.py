69ed6dd713adba07c7926afb8457260f8f221705
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-09 14:52:52 -0400

    Improved journal feature.

--- journal/journal.py
+++ journal/journal.py
@@ -1,17 +1,37 @@
 
+import os
+
 """Journal Module."""
 
 
 def load(name):
     # todo: populate from file if it exists
-    return []
+    data = []
+    filename = get_full_pathname(name)
+    if os.path.exists(filename):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+    return data
 
 
 def save(name, journal_data):
-    # todo: save journal data to journal name
-    pass
+    """Save the journal."""
+    filename = get_full_pathname(name)
+    print(f'Using : {filename}')
+    with open(filename, 'w') as fout:
+        for entry in journal_data:
+            fout.write(f'{entry}\n')
+
+
+def get_full_pathname(name):
+    """Return the full path of the named journal."""
+    filename = os.path.join('.', 'journals', f'{name}.jrl')
+    filename = os.path.abspath(filename)
+    return filename
 
 
 def add_entry(text, journal_data):
     # todo: add entry
+    journal_data.append(text)
     return None
