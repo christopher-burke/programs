444f4a0d413ac93d18c357845ac40a3da54068ef
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-11 16:56:21 -0400

    Adding lolcat services. Download feature.

---
+++ lolcat_factory/cat_service.py
@@ -0,0 +1,2 @@
+def get_cat(folder, name):
+    pass
--- lolcat_factory/main.py
+++ lolcat_factory/main.py
@@ -3,15 +3,19 @@
 """LOLCat Factory Program."""
 
 import os
+import cat_service
 
 
 def main():
     """Main function."""
     folder = output_folder()
+    download_cats(folder)
 
 
 def output_folder():
-    """Create folder for output."""
+    """Create folder for output.
+
+    :return: Full path to download folder."""
     folder = 'cat_pictures'
     base_folder = os.path.dirname(__file__)
     full_path = os.path.join(base_folder, folder)
@@ -20,6 +24,13 @@ def output_folder():
         print(f'Creating new directory {full_path}')
         os.mkdir(full_path)
     return full_path
+
+
+def download_cats(folder, count=8):
+    cat_count = count + 1
+    for i in range(1, cat_count):
+        name = f'lolcat_{i}'
+        cat_service.get_cat(folder, name)
 
 
 if __name__ == "__main__":
