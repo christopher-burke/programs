55128fa8f29ecabbc216b31d9bcba1b8dea5be07
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-12 20:53:53 -0400

    Improved docstrings.

--- lolcat_factory/cat_service.py
+++ lolcat_factory/cat_service.py
@@ -15,13 +15,22 @@ def get_cat(folder: str, name: str):
 
 
 def get_data(url: str):
-    """Get the data from the url."""
+    """Get the data from the url.
+
+    :param url: The link used to get the data.
+    :return: The raw response from requests.
+    """
     response = requests.get(url, stream=True)
     return response.raw
 
 
 def save_image(folder: str, name: str, data):
-    """Save image to disk."""
+    """Save image to disk.
+
+    :param folder: The string path to the folder.
+    :param name: The string name of the file.
+    :param data: The data to be saved.
+    """
     file_name = os.path.join(folder, name + '.jpg')
 
     with open(file_name, 'wb') as fout:
