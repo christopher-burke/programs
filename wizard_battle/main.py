aff0db1eaea3e9e7509a1ad9a3b1fa985755c042
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-17 16:02:22 -0400

    Main game loop added.

--- wizard_battle/main.py
+++ wizard_battle/main.py
@@ -4,7 +4,21 @@
 
 
 def main():
-    pass
+    game_loop()
+
+
+def game_loop():
+    while True:
+        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
+        if cmd.lower() == 'a':
+            print('attack')
+        elif cmd.lower() == 'r':
+            print('runaway')
+        elif cmd.lower() == 'l':
+            print('look around')
+        else:
+            print('Exiting...')
+            break
 
 
 if __name__ == "__main__":
