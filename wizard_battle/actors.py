31e1da1bdfddf5e0e04c8ff213b1a21a1e415f71
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-18 15:07:23 -0400

    Improving game play.

--- wizard_battle/actors.py
+++ wizard_battle/actors.py
@@ -2,12 +2,18 @@
 
 """Wizard Battle - Actors."""
 import random
+import time
 
 
 class Creature:
     def __init__(self, name: str, level: int, *args, **kwargs):
         self.name = name
         self.level = level
+        self.modifier = random.randint(5, 12)
+
+    def level_up(self, defeated_creature):
+        self.level += 1 if defeated_creature < self else 5
+        print(f'You are now level {self.level}.')
 
     def __repr__(self):
         return f'{self.__class__.__name__}(name=\'{self.name}\', level={self.level})'
@@ -16,29 +22,59 @@ class Creature:
         return f'{self.name} of level {self.level}'
         # return f'Creature: {self.name} of level {self.level}.'
 
+    def __lt__(self, other):
+        return self.level < other.level
+
+    def __le__(self, value):
+        return super().__le__(value)
+
+    def __gt__(self, value):
+        return super().__gt__(value)
+
+    def fight(self, opponent):
+        print(f'{self.name} attacks {opponent.name}!')
+        if self > opponent:
+            print(f'{self} +1 advantage.')
+
+        self_roll = self.get_defensive_roll()
+        self_roll += 1 if self > opponent else 0
+        opponent_roll = opponent.get_defensive_roll()
+
+        print(f'{self.name} roll [{self_roll}].')
+        print(f'{opponent.name} roll [{opponent_roll}].')
+
+        if self_roll >= opponent_roll:
+            print(f'{self.name} has triumphed over {opponent.name}.')
+            return True
+        else:
+            print(f'{self.name} has been DEFEATED!!!')
+            return False
+
+    def rest(self):
+        print(f'{self.name} needs time to recover...')
+        time.sleep(5)
+        print(f'{self.name} returns!')
+
+    def attack(self):
+        return bool(random.getrandbits(1))
+
     def get_defensive_roll(self):
-        return random.randint(1, 12) * self.level
+        return random.randint(1, 12) * self.level * self.modifier
 
 
 class Wizard(Creature):
     """Wizard actor in Wizard Battle."""
 
-    def attack(self, creature) -> bool:
+    def __init__(self, name, level, *args, **kwargs):
+        super().__init__(name, level, *args, **kwargs)
+
+    def get_defensive_roll(self):
+        magic_mod = 100 - (self.level % 100)
+        return super().get_defensive_roll() * magic_mod
+
+    def attack(self) -> bool:
         """Wizard Attack method."""
-        print(f'{self.name} attacks {creature.name}!')
-
-        my_roll = self.get_defensive_roll()
-        creature_roll = creature.get_defensive_roll()
-
-        print(f'{self.name} roll [{my_roll}].')
-        print(f'{creature.name} roll [{creature_roll}].')
-
-        if my_roll >= creature_roll:
-            print(f'{self.name} has triumphed over {creature.name}.')
-            return True
-        else:
-            print(f'{self.name} has been DEFEATED!!!')
-            return False
+        return super().attack()
 
 
 class Dragon(Creature):
@@ -61,5 +97,4 @@ class SmallAnimal(Creature):
 
     def get_defensive_roll(self):
         base_roll = super().get_defensive_roll()
-        print(base_roll)
         return int(base_roll // 2)
--- wizard_battle/main.py
+++ wizard_battle/main.py
@@ -20,18 +20,20 @@ def game_loop():
     creatures = [
         SmallAnimal(name='Toad',
                     level=1),
-
-        Creature(name='Tiger',
-                 level=12),
-
         SmallAnimal(name='Bat',
                     level=3),
-
+        Creature(name='Tiger',
+                 level=12),
+        Creature(name='Tiger',
+                 level=80),
         Dragon(name='Dragon',
                level=50,
                scale_thickness=75,
                fire_breathing=True),
-
+        Dragon(name='Dragon',
+               level=100,
+               scale_thickness=50,
+               fire_breathing=False),
         Wizard(name='Evil Wizard',
                level=1000),
     ]
@@ -39,19 +41,20 @@ def game_loop():
     hero = Wizard(name='Gandolf',
                   level=75)
 
+    turns = 0
     while True:
 
         active_creature = random.choice(creatures)
         print(f'A {active_creature} has appeared from a dark forest...')
 
         cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
+
         if cmd.lower() == 'a':
-            if hero.attack(active_creature):
+            if hero.fight(active_creature):
                 creatures.remove(active_creature)
+                hero.level_up(active_creature)
             else:
-                print(f'{hero.name} needs time to recover...')
-                time.sleep(5)
-                print(f'{hero.name} returns!')
+                hero.rest()
 
         elif cmd.lower() == 'r':
             print(f'{hero.name} runs away!')
@@ -59,9 +62,24 @@ def game_loop():
             print(f'{hero.name} looks around and sees:')
             for creature in creatures:
                 print(f'{creature}.')
-        else:
+
+            # Does the active creature choose to attack the Hero!
+            if active_creature in creatures and \
+                    active_creature.attack():
+                if active_creature.fight(hero):
+                    hero.rest()
+                else:
+                    creatures.remove(active_creature)
+                    hero.level_up(2)
+
+        elif cmd.lower() in ('q', 'x'):
             print('Exiting...')
             break
+
+        if not creatures:
+            print('You have won! Exiting...')
+            break
+        turns += 1
 
 
 if __name__ == "__main__":
