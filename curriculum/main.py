256289eca5d3d5fef9fb9de311b0ec276223f7ed
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-31 13:58:22 -0400

    Added Curriculum Generator program.

---
+++ curriculum/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Curriculum Generator,
+
+
+Idea source: https://pybit.es/codechallenge55.html
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
